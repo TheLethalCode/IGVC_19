33569f744ef0731bf06e8955986c383a404924b2
ShreyanshDarshan <shreyanshdarshan1@gmail.com>
2019-05-09 20:41:03 +0530

    Add object detection header
    Adds function that creates object mask on input image. Works fast on multicore CPU.

---
+++ vision/objectdetection.h
@@ -0,0 +1,82 @@
+
+#include"opencv2/highgui/highgui.hpp"
+#include"opencv2/imgproc/imgproc.hpp"
+#include"opencv2/core/core.hpp"
+#include<iostream>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <string>
+
+using namespace std;
+using namespace cv;
+
+#define alpha 50
+
+#define threshb 30
+#define threshg 70
+#define threshr 70
+
+#define threshbW 210
+#define threshgW 220
+#define threshrW 210
+
+int max (int a, int b)
+{
+	return (a>b)?a:b;
+}
+
+int min (int a, int b)
+{
+	return (a<b)?a:b;
+}
+
+float mapped (float a, float ist, float iend, float ost, float oend)
+{
+	return (a-ist)*(oend-ost)/(iend-ist) + ost;
+}
+
+VideoCapture vid("IGVC.mp4");
+
+typedef cv::Point3_<uint8_t> Pixel;
+
+Mat &CreateObjectMask(Mat &img) 
+{
+	img.forEach<Pixel>([](Pixel &p, const int * position) -> void 
+	{
+	    Pixel img2;
+	    bool flag = false;
+	    (img2.x) = (p.x);		//blue
+		(img2.y) = p.y+mapped(p.y, 0, 255, alpha, 0);		//green
+		(img2.z) = max(p.z-mapped(p.z, 0, 255, alpha, 0), 0); 		//red2
+		p = img2;
+		if (img2.y == max(max(p.x, p.y), p.z)) 
+		{
+		 	if (img2.x == min(min(p.x, p.y), p.z))
+		 	{
+				if (	
+						!((p.x < threshb || p.x > threshbW) && 
+						(p.y < threshg || p.y > threshgW) && 
+						(p.z < threshr || p.z > threshrW) )
+					)
+				{ 
+		 			flag = true;
+				}
+		 	}
+		}
+		if (!flag)
+		{
+			p.x = 255;
+			p.y = 255;
+			p.z = 255;
+		}
+		else
+		{
+			p.x = 0;
+			p.y = 0;
+			p.z = 0;	
+		}
+	});
+	erode(img, img, Mat(), Point(-1, -1), 4, 1, 1);
+	medianBlur(img, img, 5);
+	return img;
+}
