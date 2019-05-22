1cfa82375fd7b2fee3962bea193a99d5d46c7b9e
vib2810 <vib2810@gmail.com>
2019-05-23 02:47:30 +0530

    Added script for running imu and lidar, changed print rate of imu node, To run the script -- bash run_sensors.sh

---
+++ run_sensors.sh
@@ -0,0 +1,105 @@
+# sudo chmod 777 /dev/tty*
+# printf "sudo chmod done"
+# off = $0
+
+# # cam=$(rosparam get cam)
+# # imu=$(rosparam get imu)
+# # lid=$(rosparam get lid)
+# # done
+# terminator --command bash\ imu.sh\ &  
+# # PID = $!
+# # printf "%d" PID
+trap cleanup 2
+port=0
+off=0
+rosparam set lid 0
+rosparam set imu 0
+rosparam set cam 0
+
+cleanup()
+{
+  echo "Caught Signal ... cleaning up."
+  killall -9 error
+  sleep 1
+  killall -9 hokuyo_node
+  sleep 1
+  killall -9 vn_ins
+  sleep 1
+  killall -9 nodelet
+  sleep 1
+  exit 1
+}
+
+switch_port () {
+    if [ $port \> 1 ]
+    then
+        port=0
+        printf "hello"
+    fi
+    temp="/dev/ttyACM$port"
+    rosparam set port $temp
+    printf "port: %d concat:%s\n" $port $temp
+    port=$((port+1))
+}
+
+sudo chmod 777 /dev/tty*
+rosrun sensor_status error &
+
+roslaunch vn_ins module.launch &
+sleep 1
+# sudo sysctl -w net.core.rmem_max=1048576 net.core.rmem_default=1048576 
+# roslaunch pointgrey_camera_driver camera.launch &
+# sleep 1
+roslaunch hokuyo_node hokuyo_test.launch &
+sleep 1
+
+# cam=$(rosparam get cam)
+# imu=$(rosparam get imu)
+# lid=$(rosparam get lid)
+
+while [ 1 ]
+do
+    if [ $imu == "0" ]
+    then
+        printf "Killing IMU !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        killall -9 vn_ins
+        sleep 1
+        sudo chmod 777 /dev/tty*
+        printf "Relaunching IMU -----------------------------------------------------------------------"
+        roslaunch vn_ins module.launch &
+        sleep 1
+    fi
+    if [ $lid == "0" ]
+    then
+      printf "Killing Lidar !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+      killall -9 hokuyo_node
+      sleep 1
+      sudo chmod 777 /dev/tty*
+      printf "Relaunching Lidar -----------------------------------------------------------------------"
+      switch_port
+      roslaunch hokuyo_node hokuyo_test.launch &  
+      sleep 1
+    fi
+
+    # if [ $cam == "0" ]
+    # then
+    #   printf "Killing camera !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+    #   killall -9 nodelet
+    #   sleep 1
+    #   sudo chmod 777 /dev/tty*
+    #   printf "Relaunching camera -----------------------------------------------------------------------"
+    #   roslaunch pointgrey_camera_driver camera.launch &
+    #   sleep 2
+    # fi
+        
+    imu=$(rosparam get imu)
+    # cam=$(rosparam get cam)
+    lid=$(rosparam get lid)
+    if [ $lid == "1" ] && [ $imu == "1" ] 
+    then
+        printf "ALL connected\n"
+    fi
+    sleep 5
+done
+
+
