29870d00f3b410b49807549b86e675846fd67a3f
manthan99 <manthanhemangpatel@gmail.com>
2019-06-04 10:21:17 +0530

    final working code, don't PUSH without review

--- teb_params_no_mans_land.sh
+++ teb_params_no_mans_land.sh
@@ -1,2 +1,3 @@
 rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS xy_goal_tolerance 1
 rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS yaw_goal_tolerance 0.7853981634 #45 degrees on either side
+rosrun dynamic_reconfigure dynparam set /master costmap_median_blur 7
--- teb_params_vision.sh
+++ teb_params_vision.sh
@@ -1,2 +1,4 @@
 rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS xy_goal_tolerance 0.3
 rosrun dynamic_reconfigure dynparam set /move_base/TebLocalPlannerROS yaw_goal_tolerance 0.3 #18 degrees on either side
+cmb = $(rosparam get /master/costmap_median_blur)
+rosrun dynamic_reconfigure dynparam set /master costmap_median_blur $cmb
